88a662f56b2c7f93243a13ac9282e24905be8ea4
ixboy <ismaelixboy@gmail.com>
2022-03-11 19:33:09 +0300

    refactor code and fix all linter errors

--- bin/game.rb
+++ bin/game.rb
@@ -1,22 +1,26 @@
 #!/usr/bin/env ruby
-require_relative '../lib/players.rb'
+require_relative '../lib/players'
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Style/InfiniteLoop
 
 class Game < Players
   def print_board(b)
     a = @available
     puts "\n           Available            TIC-TAC_TOE\n             moves\n\n" \
-     "          #{a[0]} | #{a[1]}  | #{a[2]}            #{b[0]}  |  #{b[1]}  | #{b[2]}\n" \
-     "       ****************      ******************\n"  \
-     "          #{a[3]} | #{a[4]}  | #{a[5]}            #{b[3]}  |  #{b[4]}  | #{b[5]}\n" \
-     "       ****************      ******************\n"  \
-     "          #{a[6]} | #{a[7]}  | #{a[8]}            #{b[6]}  |  #{b[7]}  | #{b[8]}\n\n"   \
-     "       -----------------------------------------------------\n"
-  end 
+         "          #{a[0]} | #{a[1]}  | #{a[2]}            #{b[0]}  |  #{b[1]}  | #{b[2]}\n" \
+         "       ****************      ******************\n"  \
+         "          #{a[3]} | #{a[4]}  | #{a[5]}            #{b[3]}  |  #{b[4]}  | #{b[5]}\n" \
+         "       ****************      ******************\n"  \
+         "          #{a[6]} | #{a[7]}  | #{a[8]}            #{b[6]}  |  #{b[7]}  | #{b[8]}\n\n"   \
+         "       -----------------------------------------------------\n"
+  end
 
   def start_game
     while true
       Utilities.clear_terminal
-      player_name = Utilities.ask_the_name("Player-1")
+      player_name = Utilities.ask_the_name('Player-1')
       difficult = Utilities.difficulty_level
       sleep(2)
       Utilities.clear_terminal
@@ -27,41 +31,43 @@ class Game < Players
         flag = false
         Utilities.players_msg(player_name)
         hum_move = false
-        until hum_move
-          hum_move = get_human_spot(player_name)
-        end
+        hum_move = human_spot(player_name) until hum_move
         sleep(1)
         Utilities.clear_terminal
         Utilities.user_moves(player_name, hum_move)
         print_board(@board)
         sleep(1)
-        if !game_is_over(@board) && !tie(@board)
-          get_computer_default if !difficult
-          get_computer_hard if difficult
-          print_board(@board)
-          sleep(1)
-          flag = true
-        end
+        next unless !game_is_over(@board) && !tie(@board)
+
+        computer_default unless difficult
+        computer_hard if difficult
+        print_board(@board)
+        sleep(1)
+        flag = true
       end
+      Utilities.clear_terminal
+      print_board(@board)
       if game_is_over(@board)
-        Utilities.clear_terminal
-        print_board(@board)
-        flag ? Utilities.winning_msg("COMPUTER") : Utilities.winning_msg("YOU")
+        flag ? Utilities.winning_msg('COMPUTER') : Utilities.winning_msg('YOU')
       else
-        Utilities.clear_terminal
-        print_board(@board)
         Utilities.its_a_tie_msg
       end
-      if game_is_over(@board) || tie(@board)
-        repeat = Utilities.play_again?
-        sleep(1)
-        Utilities.thanks unless repeat
-        break unless repeat
-        reset
-      end
+      next unless game_is_over(@board) || tie(@board)
+
+      repeat = Utilities.play_again?
+      sleep(1)
+      Utilities.thanks unless repeat
+      break unless repeat
+
+      reset
     end
   end
 end
 
 game = Game.new
 game.start_game
+
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Style/InfiniteLoop
--- lib/utils.rb
+++ lib/utils.rb
@@ -1,74 +1,87 @@
 require 'colorize'
 
 module Utilities
-    def self.clear_terminal
-      if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
-        system('cls')
-      else
-        system('clear')
-      end
+  def self.clear_terminal
+    if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
+      system('cls')
+    else
+      system('clear')
     end
+  end
 
-    def self.user_moves(user, spot)
-        puts "\n             #{user} played position number #{spot}".light_magenta
-    end
+  def self.user_moves(user, spot)
+    puts "\n             #{user} played position number #{spot}".light_magenta
+  end
 
-    def self.winning_msg(user)
-        5.times { puts "\n" }
-        if user == "COMPUTER"
-            puts "\n               I AM SORRY THE #{user} WON! TRY AGAIN!!! \n\n".red
-        else
-            puts "\n               CONGRATULATIONS #{user} WON!!! \n\n".light_green
-        end
+  def self.winning_msg(user)
+    5.times { puts "\n" }
+    if user == 'COMPUTER'
+      puts "\n               I AM SORRY THE #{user} WON! TRY AGAIN!!! \n\n".red
+    else
+      puts "\n               CONGRATULATIONS #{user} WON!!! \n\n".light_green
     end
+  end
 
-    def self.difficulty_level
-        puts "\n\n             Enter 1 for the Computer to play a bit hard against you\n\n             Enter any key for Computer to play easy against you".light_yellow
-        level = gets.chomp
-        # @play_mode = play_mode
-        if level == '1'
-          puts "\n\n             Computer play hard mode activated!.  You can still win if you play nicer".light_red
-        else
-          puts "\n\n             Computer play easy mode activated!".green
-        end
-        level == '1' ? true : false
+  def self.difficulty_level
+    puts "\n\n             Enter 1 for the Computer to play a bit hard against you\n\n             Enter any key for Computer to play easy against you".light_yellow
+    level = gets.chomp
+    # @play_mode = play_mode
+    if level == '1'
+      puts "\n\n             Computer player, hard mode activated!.  You can still win if you play nicer".light_red
+    else
+      puts "\n\n             Computer player, easy mode activated!".green
     end
+    level == '1'
+  end
 
-    def self.its_a_tie_msg
-        puts "\n               IT'S A DRAW!!! TRY AGAIN!".yellow
-    end
+  def self.its_a_tie_msg
+    puts "\n               IT'S A DRAW!!! TRY AGAIN!".yellow
+  end
 
-    def self.thanks
-        3.times { puts "\n" }
-        puts '      **************You terminated the Game!!!!************'.light_green
-        puts '      ***Thank You for playing it, I hope you enjoyed it***'.light_green
-        puts '      *****You can always come back for more games :D******'.light_green
-        3.times { puts "\n" }
-        puts '      ***********Created by: Ismael Antonio**************'.light_yellow
-    end
+  def self.thanks
+    3.times { puts "\n" }
+    puts '      **************You terminated the Game!!!!************'.light_green
+    puts '      ***Thank You for playing it, I hope you enjoyed it***'.light_green
+    puts '      *****You can always come back for more games :D******'.light_green
+    2.times { puts "\n" }
+    puts '      ***********Created by: Ismael Antonio**************'.light_yellow
+  end
 
-    def self.play_again?
-        puts "\n               DO YOU WANT TO PLAY AGAIN!?".yellow
-        puts "\n\n             Enter >>>'y'<<<  to play again.\n             Enter any key to end the game".light_blue
-        repeat = gets.chomp.upcase
-        repeat == "Y" ? true : false
-    end
+  def self.play_again?
+    puts "\n               DO YOU WANT TO PLAY AGAIN!?".yellow
+    puts "\n\n             Enter >>>'y'<<<  to play again.\n             Enter any key to end the game".light_blue
+    repeat = gets.chomp.upcase
+    repeat == 'Y'
+  end
 
-    def self.ask_the_name(player)
-        puts "\n\n               #{player}, please write your name\n\n"
-        puts ""
-        player_name = gets.chomp
-        if player_name.length >= 2
-          puts "\n\n                             Welcome #{player_name.upcase}".light_red
-        else
-          puts '               Please enter a correct name'
-          puts '               Name should be atleast two characters'
-          player_name = ask_the_name(player)
-        end
-        player_name
+  def self.ask_the_name(player)
+    puts "\n\n               #{player}, please write your name\n\n"
+    player_name = gets.chomp
+    if player_name.length >= 2
+      puts "\n\n                             Welcome #{player_name.upcase}".light_red
+    else
+      puts '               Please enter a correct name'
+      puts '               Name should be atleast two characters'
+      player_name = ask_the_name(player)
     end
+    player_name
+  end
 
-    def self.players_msg(player)
-        puts "\n\n         #{player} your symbol is >>>'O'<<< and the Computer is >>>'X'<<<.\n                 it's your turn to play now...".light_yellow
-    end
+  def self.players_msg(player)
+    puts "\n\n         #{player} your symbol is >>>'O'<<< and the Computer is >>>'X'<<<.\n                 it's your turn to play now...".light_yellow
+  end
+
+  def self.choice_msg(player)
+    puts "\n          #{player} Please Select available spot between 0 and 8.".light_yellow
+  end
+
+  def self.num?(num)
+    !num.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/).nil?
+  end
+
+  def self.validate_number(num)
+    copy = num
+    num = num?(num) ? copy.to_i : 10
+    num.negative? || num > 8 ? nil : num
+  end
 end
